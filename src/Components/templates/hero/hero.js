bdd37c6668ab848f60219cee7c37405068b106a7
gabimancini <gabimancinir@gmail.com>
2023-08-13 18:04:10 -0400

    Add sticky notes

--- src/Components/templates/hero/hero.js
+++ src/Components/templates/hero/hero.js
@@ -1,18 +1,21 @@
 import Img from "../../atoms/img/Img";
 import HeroImg from "../../../assets/hero.svg";
+import StickyImg from "../../../assets/stickyImg.svg";
 const Hero = () => {
     return (
         <section id="hero">
-        <div className="container">
-        <div>
-            <h2>Hola!</h2>
-            <h1>Mi nombre es Alejandro Ritta</h1>
-            <h1>Soy diseñador web</h1>
+            <div className="container">
+                <div className="hero_text">
+                    <h2>Hello!</h2>
+                    <h1>Mi name is<br /> <span>Alejandro Ritta</span></h1><br />
+                    <h1> I’m a UX Designer</h1>
+                </div>
+                <div className="hero_img">
+                    <Img src={HeroImg} alt="Hero" className="hero_img" />
+                </div>
+                <div className="sticky"><p>desktop<br/>
+                1440x1024px</p></div>
             </div>
-            <div>
-<Img src={HeroImg} alt="Hero" className="hero_img"/>
-            </div>
-        </div>
         </section>
     )
 }
