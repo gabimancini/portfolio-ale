1514a55e6356b3fc972ec405cc91baae0bf8d1c8
gabimancini <gabimancinir@gmail.com>
2023-08-28 14:35:13 -0400

    Add padding to stickies padding on mobile

--- src/Components/templates/skills/skills.js
+++ src/Components/templates/skills/skills.js
@@ -21,8 +21,8 @@ const Skills = () => {
     const List = ["Figma  100%", "Justinmind  80%", "Agile methodology 90%", "Adobe XD 100%", "Lenguage 60% ", "Google Presentation 100%"]
     return (
         <section id="skills">
-            <div className="container d-flex p-y-100 p-x-20 wrap justify-space-between">
-                <div className="container__col-lg-6 d-flex align-center wrap direction-md-col sticky_kill m-auto">
+            <div className="container d-flex p-y-100 p-x-20 wrap justify-space-between padding-md-10">
+                <div className="container__col-lg-6 d-flex align-center wrap direction-md-col sticky_kill m-auto m-y-30">
                     {Stickies.map((sticky, index) => (
                         <Sticky key={index} title={sticky.title} text={sticky.text} className={`bg-${sticky.bg} sticky_skills h-200 p-10 font-size-1 m-y-10 width-md-271 padding-md-40`} />
                     )
@@ -31,7 +31,7 @@ const Skills = () => {
                 <div className="container__col-lg-5">
                 <h2 className="font-size-h2 text-md-center">My skills</h2>
                     <ul className="d-flex wrap">
-                        {List.map((item, index)=><li key={index} className="skills_list font-size-1_5 p-10 w-50per font-size-md-14">{item}</li>)}
+                        {List.map((item, index)=><li key={index} className="skills_list font-size-1_5 p-10 w-50per font-size-md-14 padding-md-0 m-y-10">{item}</li>)}
                     </ul>
                 </div>
             </div>
