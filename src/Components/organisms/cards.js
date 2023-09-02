b4109de83d0f346df8485e876cd3f4d808892561
gabimancini <gabimancinir@gmail.com>
2023-09-02 18:49:03 -0400

    Fix imgs in project

--- src/Components/organisms/cards.js
+++ src/Components/organisms/cards.js
@@ -2,7 +2,7 @@ import Img from "../atoms/img/Img";
 const Cards = ({btnText,text,src,alt,className})=>{
     return(
         <div className={`card w-1/2 container__col-sm-12 container__col-lg-5 bg-browser1 p-y-40 p-x-5 position-relative h-400 direction-column justify-end d-flex`}>
-        <div className="card_img position-absolute top-40 left-0"> <Img src={src} alt={alt} className={className} /></div>
+        <div className="card_img position-absolute top-35 left-3 w-98per h-271"> <Img src={src} alt={alt} className={className} /></div>
         <div>
              <button>{btnText}</button>
              <span>{text}</span>
