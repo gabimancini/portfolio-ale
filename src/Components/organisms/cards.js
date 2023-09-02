46e5c7b4e7114457cbbd197f38504d8da0308bb6
gabimancini <gabimancinir@gmail.com>
2023-09-02 18:18:54 -0400

    Fix proyects imgs

--- src/Components/organisms/cards.js
+++ src/Components/organisms/cards.js
@@ -1,8 +1,8 @@
 import Img from "../atoms/img/Img";
 const Cards = ({btnText,text,src,alt,className})=>{
     return(
-        <div className={`card w-1/2 container__col-sm-12 container__col-lg-5 bg-browser1 p-y-40 p-x-5`}>
-        <Img src={src} alt={alt} className={className} />
+        <div className={`card w-1/2 container__col-sm-12 container__col-lg-5 bg-browser1 p-y-40 p-x-5 position-relative h-400 direction-column justify-end d-flex`}>
+        <div className="card_img position-absolute top-40 left-0"> <Img src={src} alt={alt} className={className} /></div>
         <div>
              <button>{btnText}</button>
              <span>{text}</span>
--- src/Components/pages/projects.js
+++ src/Components/pages/projects.js
@@ -1,19 +1,16 @@
 import Cards from "../organisms/cards";
 import Gabriela from '../../assets/gabriela.svg';
 import Pet from '../../assets/pet.svg';
-import { useState } from 'react';
 import Devmagister from '../../assets/devmagister.svg';
 import Steak from '../../assets/steak.svg';
-const Projects = () => {
-    const [expanded, setExpanded] = useState(false)
-
-    let browser1 = "browser1"
+const Projects = () => { 
+ 
     let projects = [
         {
             button1: "Web site",
             button2: "Figma",
             img: Gabriela,
-            bg: browser1
+            bg: "browser1"
         },
         {
             button1: "E-commerce",
@@ -34,12 +31,6 @@ const Projects = () => {
         }
 
     ]
-    const onClickHandle = () => {
-        setExpanded(!expanded)
-        projects.splice(0, 2);
-        console.log(window.innerWidth)
-
-    }
 
     return (
         <section id="projects">
@@ -49,21 +40,14 @@ const Projects = () => {
                     <p className="font-size-paragraph color-black-aplha-6">These projects I've worked on are just the tip of the iceberg, but I'm proud of each <br /> and every one of them.
                     </p>
                 </div>
-                <div className="d-flex wrap justify-space-around p-x-20">
+                <div className="d-flex wrap justify-space-around p-x-35">
 
-                    {expanded ? projects.map((project, index) => (
-
-                        <Cards key={index} btnText={project.button1} text={project.text} src={project.img} className="text-left bg-browser1" />
-                    )) : projects.slice(0, 2).map((project, index) => (
+                   { projects.map((project, index) => (
 
                         <Cards key={index} btnText={project.button1} text={project.text} src={project.img} className="text-left bg-browser1" />
-                    )
-                    )
-                    }
-                </div>{
-                    window.innerWidth < 780 ?
-                        <button onClick={onClickHandle}>{expanded ? "show less" : "show more"}</button> : ""
-                }
+                    ))
+                   }
+                </div>
 
             </div>
         </section>
